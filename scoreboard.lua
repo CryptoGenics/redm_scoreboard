f7fdbaca1cb891aa0a6f21bf87a90e72b695c8bd
CryptoGenics <45253987+CryptoGenics@users.noreply.github.com>
2020-09-03 14:23:15 -0500

    Security updates

--- scoreboard.lua
+++ scoreboard.lua
@@ -1,24 +1,13 @@
+local MyKey
+
 Citizen.CreateThread(function()
     open = false
     while true do
         Wait(0)
         if IsControlPressed(0, Config.key) and IsInputDisabled(0) then
             if not open then
-                if Config.Ping or Config.RPnames then
-                    TriggerServerEvent("redm_scoreboard:GetBoard", GetPlayers())
-                else
-                    local players = {}
-                    ptable = GetPlayers()
-                    for _, i in ipairs(ptable) do
-                        table.insert(players, 
-                        '<tr style="color: rgb(255, 255, 255); font-weight: 500;"><td>' .. GetPlayerServerId(i) .. '</td><td>' ..GetPlayerName(i)..'</td></tr>'
-                        )
-                    end
-                    DisplayHud(false)
-                    DisplayRadar(false)
-                    SendNUIMessage({ text = table.concat(players) })
-                end
-
+                MyKey = GetGameTimer()
+                TriggerServerEvent("redm_scoreboard:GetBoard", GetPlayers(), MyKey)
                 open = true
                 while open do
                     Wait(0)
@@ -38,10 +27,28 @@ Citizen.CreateThread(function()
 end)
 
 RegisterNetEvent("redm_scoreboard:Show")
-AddEventHandler("redm_scoreboard:Show", function(html)
-    DisplayHud(false)
-    DisplayRadar(false)
-	SendNUIMessage({ text = html })
+AddEventHandler("redm_scoreboard:Show", function(html, key)
+    if MyKey == key then
+        DisplayHud(false)
+        DisplayRadar(false)
+        local players = {}
+        for _,v in pairs(html) do
+            if Config.Ping then
+                local id = tonumber(v[1])
+                local name = v[2]
+                local ping = tonumber(v[3])
+                table.insert(players, 
+                '<tr><td>'..id..'</td><td>'..name..'</td><td>'..ping..'<small>ms</small></td></tr>')
+            else
+                local id = tonumber(v[1])
+                local name = v[2]
+                table.insert(players, 
+                '<tr><td>'..id..'</td><td>'..name..'</td></tr>'
+                )
+            end
+        end
+        SendNUIMessage({ text = table.concat(players) })
+    end
 end)
 
 function GetPlayers()
